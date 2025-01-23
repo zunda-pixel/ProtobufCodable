5b647c9978749bee353775c70b822c6abaa2daea
zunda.dev@gmail.com <zunda.dev@gmail.com>
2025-01-23 23:15:18 +0900

    add func encode(_ value: String) throws

--- Sources/ProtobufCodable/Encoding/EnumEncoder.swift
+++ Sources/ProtobufCodable/Encoding/EnumEncoder.swift
@@ -26,8 +26,12 @@ final class EnumEncoder: AbstractEncodingNode, SingleValueEncodingContainer {
         self.encodedValue = value
     }
 
+    func encode(_ value: String) throws {
+        self.encodedValue = value
+    }
+    
     func encode<T>(_ value: T) throws where T : Encodable {
-        throw EncodingError.unsupported(value, "Enum must use `RawValue` of `Int`, `Int64` or `Int32`, not \(T.self)", codingPath: codingPath)
+        throw EncodingError.unsupported(value, "Enum must use `RawValue` of `String`, `Int`, `Int64` or `Int32`, not \(T.self)", codingPath: codingPath)
     }
 }
 
